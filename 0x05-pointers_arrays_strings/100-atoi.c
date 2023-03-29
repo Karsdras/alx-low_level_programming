2334147943b9fdcb241be43b1998425356dc5e35
karsdras <idris.kazeem@outlook.com>
2023-03-29 14:54:44 -0700

    converts strings to integers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,40 +9,22 @@
 int _atoi(char *s)
 {
 	int i = 0;
-    	int c = 0;
-    	int sidn = 1;
-    	while (s[i])
-    	{
-		if (s[i] == 43)
-            		sidn *= 1;
-        	else if (s[i] == 45)
-            		sidn *= -1;
-        	if (s[i] >= 48 && s[i] <= 57)
-		{
-            		c = c * 10 + (s[i] - 48);
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-			       break;
-		}	
-        	i++;
-    	}
-    	return sidn*c;
-}
-/*	int i, m, n, o;
+	unsigned int c = 0;
+	int sidn = 1;
 
-	i = n = 0;
-	o = 1;
-	while ((*(s + i) < '0' || *(s + i) > '9') && (*(s + i) != '\0'))
+	while (s[i])
 	{
-		if (*(s + i) == '-')
-			o *= -1;
+		if (s[i] == 43)
+			sidn *= 1;
+		else if (s[i] == 45)
+			sidn *= -1;
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			c = c * 10 + (s[i] - 48);
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
 		i++;
 	}
-	m = 1;
-	while ((*(s + m) >= '0') && (*(s + m) <= '9'))
-	{
-		n = n * 10 + o * (*(s + m) - '0');
-		m++;
-	}
-	return (n);
+	return (sidn * c);
 }
-*/
