12b802feadfb4951cd869625e03cd1cf97967320
karsdras <idris.kazeem@outlook.com>
2023-03-29 14:19:51 -0700

    converts strings to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,7 @@
 
 int _atoi(char *s)
 {
-	 int i = 0;
-    int c = 0;
-    int sidn = 1;
-    while (s[i])
-    {
-        if (s[i] == 43)
-            sidn *= 1;
-        else if (s[i] == 45)
-            sidn *= -1;
-        if (s[i] >= 48 && s[i] <= 57)
-            c = c * 10 + (s[i] - 48);
-        i++;
-    }
-    return sidn*c;
-}
-/*	int i, m, n, o;
+	int i, m, n, o;
 
 	i = n = 0;
 	o = 1;
@@ -41,4 +26,3 @@ int _atoi(char *s)
 	}
 	return (n);
 }
-*/
